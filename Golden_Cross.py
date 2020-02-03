a9d26635b623544b0857c73149d99e92ea261473
G <54356938+BG4Finance@users.noreply.github.com>
2020-02-03 18:36:55 +0100

    Golden Cross

---
+++ Golden_Cross.py
@@ -0,0 +1,60 @@
+import alpaca_trade_api as alp
+import numpy as np
+import pandas as pd
+import statistics
+import sys
+import time
+import datetime as dt
+from datetime import datetime, timedelta
+from pytz import timezone
+from alpaca_keys import keyid, secretkey, baseurl
+
+id = keyid
+key = secretkey
+url = baseurl
+
+class alpaca_GB:
+    def __init__(self, user = id, password = key, url = url, time_format = "%m/%d/%Y, %H:%M:%S"):
+        self.id = user
+        self.key = password
+        self.api = alp.REST(key_id = self.id, secret_key = self.key, base_url = url)
+        self.api.list_positions()
+        self.account = self.api.get_account()
+        self.account.status
+        self.time_format = time_format
+
+    def status(self):
+        today = (dt.datetime.now().strftime(self.time_format))
+        print(today)
+
+        equity = float(self.account.equity)
+        margin_multiplier = float(self.account.multiplier)
+        print('Margin multiple: %s' %(margin_multiplier) + 'X')
+        tot_buy_pow = margin_multiplier * equity
+        tot_buy_pow_pc = tot_buy_pow * 100/equity
+        print('Actual total buying power = {0}, {1}%'.format(tot_buy_pow, tot_buy_pow_pc))
+
+
+    def get_data(self, stock = 'TSLA', time = 'day'):
+        now = (dt.datetime.now()).date().strftime(self.time_format)
+        data = self.api.get_barset(stock ,timeframe = time, limit=80, end=now)
+
+
+    def sma(self, short_obs, long_obs):
+        sma_st = 0
+        sma_lt = 0
+        for i in range(1,long_obs):
+            sma_lt = (smalt + data['TSLA'][-i].c)
+            sma_lt=round(sma_lt/long_obs,2)
+
+        for i in range(1,short_obs):
+            sma_st = (sma_st + data['TSLA'][-i].c)
+            sma_st=round(sma_st/short_obs,2)
+
+        print('Simple moving average, %s: %s; %s:%s.' %(long_obs, sma_lt, short_obs, sma_st))
+        if sma4 > sma6 :
+            print('BUY PHASE')
+        elif sma6 > sma4:
+            print('SELL PHASE')
+        else:
+            print('GOLD CROSS IN COURSE')
