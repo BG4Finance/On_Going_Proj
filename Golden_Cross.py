aa3a63d344be983c26669a463d58cb9ea762a74e
G <54356938+BG4Finance@users.noreply.github.com>
2020-02-05 11:34:58 +0100

    Update Golden_Cross.py

--- Golden_Cross.py
+++ Golden_Cross.py
@@ -20,7 +20,8 @@ class alpaca_GB:
         self.id = user
         self.key = password
         self.api = alp.REST(key_id = self.id, secret_key = self.key, base_url = url)
-        self.api.list_positions()
+        self.portfolio = self.api.list_positions()
+        self.portfolio
         self.account = self.api.get_account()
         self.account.status
         self.time_format = time_format
@@ -44,33 +45,55 @@ class alpaca_GB:
             print('{} Not available'.format(asset))
 
 
-    def get_data(self, stock = 'TSLA', time = 'day'):
+    def get_data(self, stock = 'TSLA', time = 'day', print_data = False):
         now = (dt.datetime.now()).date().strftime(self.time_format)
-        data = []
+        self.data = []
         for ass in stock:
             ass_data = self.api.get_barset(stock ,timeframe = time, limit=80, end=now)[ass]
             close = ass_data[-1].c
             print(ass)
-            print(ass_data)
-            print('Close: {}'.format(close))
-            data.append(ass_data)
+            if print_data == True:
+                print(ass_data)
+            print('Close: {0}; Vol: {1}'.format(close, ass_data[-1].v))
+            self.data.append(ass_data)
 
-
-    def sma(self, short_obs, long_obs):
-        sma_st = 0
-        sma_lt = 0
-        for i in range(1,long_obs):
-            sma_lt = (smalt + data['TSLA'][-i].c)
+    #MIX of data plus mov avg.
+    def sma_check(self, stock = 'TSLA', time = 'day', short_obs = 40, long_obs = 60):
+        now = (dt.datetime.now()).date().strftime(self.time_format)
+        self.data = []
+        for ass in stock:
+            ass_data = self.api.get_barset(stock ,timeframe = time, limit=long_obs+2, end=now)[ass]
+            close = ass_data[-1].c
+            print(ass)
+            print('Close: {0}; Vol: {1}'.format(close, ass_data[-1].v))
+            self.data.append(ass_data)
+            #Mov avg calculation and evaluation for strategy
+            sma_st = 0
+            sma_lt = 0
+            for i in range(1,long_obs):
+                sma_lt += ass_data[-i].c
             sma_lt=round(sma_lt/long_obs,2)
 
-        for i in range(1,short_obs):
-            sma_st = (sma_st + data['TSLA'][-i].c)
+            for i in range(1,short_obs):
+                sma_st = (sma_st + ass_data[-i].c)
             sma_st=round(sma_st/short_obs,2)
 
-        print('Simple moving average, %s: %s; %s:%s.' %(long_obs, sma_lt, short_obs, sma_st))
-        if sma4 > sma6 :
-            print('BUY PHASE')
-        elif sma6 > sma4:
-            print('SELL PHASE')
-        else:
-            print('GOLD CROSS IN COURSE')
+            print('Simple moving average: {0}obs: {1}; {2}obs: {3}.'.format(long_obs, sma_lt, short_obs, sma_st))
+            if sma_st > sma_lt :
+                self.status = 'buy'
+                print('BUY PHASE')
+            elif sma_lt > sma_st:
+                self.status = 'sell'
+                print('SELL PHASE')
+            else:
+                self.status = 'wait'
+                print('GOLD CROSS IN COURSE')
+
+    def sma_trade (self):
+        pass
+
+        # Get a list of all of our positions.
+
+# Print the quantity of shares for each position.
+#for position in portfolio:
+#    print("{} shares of {}".format(position.qty, position.symbol))
